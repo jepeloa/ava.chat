d838f3c6b77247ec239f61f6cba574a2d328037a
jepeloa <javier.epeloa@mapplics.com>
2024-11-12 12:13:48 -0300

    app name

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -197,7 +197,7 @@
 </script>
 
 <svelte:head>
-	<title>'{$WEBUI_NAME}'</title>
+	<title>'Ava - Avalian'</title>
 	<link crossorigin="anonymous" rel="icon" href="{WEBUI_BASE_URL}/static/favicon.png" />
 
 	<!-- rosepine themes have been disabled as it's not up to date with our latest version. -->
